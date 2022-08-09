f88b4eb69adb50c52587ee1d5cff15e9fd3775fc
gabriel <puppizin@gmail.com>
2022-08-09 10:58:02 -0300

    home buttons

--- minigames/AppDelegate.swift
+++ minigames/AppDelegate.swift
@@ -6,15 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = GameSelectViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
--- minigames/GameSelectViewController.swift
+++ minigames/GameSelectViewController.swift
@@ -5,4 +5,79 @@
 //  Created by Gabriel Puppi on 08/08/22.
 //
 
-import Foundation
+import SnapKit
+import UIKit
+
+
+
+class GameSelectViewController: UIViewController {
+    
+    
+    lazy var minigame1Button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Minigame1", for: .normal)
+        
+        return button
+    }()
+    
+    lazy var minigame2Button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Minigame2", for: .normal)
+        return button
+    }()
+    
+    lazy var minigame3Button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Minigame3", for: .normal)
+        return button
+    }()
+    
+    lazy var minigame4Button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Minigame4", for: .normal)
+        return button
+    }()
+    
+  override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        presentButtons()
+    }
+    
+    func presentButtons() {
+        addButtons()
+        setupButtons()
+  }
+    
+    
+    func addButtons(){
+        self.view.addSubview(minigame1Button)
+        self.view.addSubview(minigame2Button)
+        self.view.addSubview(minigame3Button)
+        self.view.addSubview(minigame4Button)
+    }
+    
+    func setupButtons(){
+        minigame1Button.backgroundColor = .black
+        minigame2Button.backgroundColor = .black
+        minigame3Button.backgroundColor = .black
+        minigame4Button.backgroundColor = .black
+        
+        minigame1Button.snp.makeConstraints { make in
+            make.top.equalTo(self.view.frame.maxY).offset(200)
+            make.centerX.equalTo(self.view.frame.midX)
+        }
+        minigame2Button.snp.makeConstraints { make in
+            make.top.equalTo(minigame1Button.snp.bottom).offset(50)
+            make.centerX.equalTo(self.view.frame.midX)
+        }
+        minigame3Button.snp.makeConstraints { make in
+            make.top.equalTo(minigame2Button.snp.bottom).offset(50)
+            make.centerX.equalTo(self.view.frame.midX)
+        }
+        minigame4Button.snp.makeConstraints { make in
+            make.top.equalTo(minigame3Button.snp.bottom).offset(50)
+            make.centerX.equalTo(self.view.frame.midX)
+        }
+    }
+}
