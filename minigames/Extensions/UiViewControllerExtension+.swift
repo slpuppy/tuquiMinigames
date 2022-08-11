b4dd75af89b506a054f525a97645e006daba7818
Eros Maurilio <eros.maurilio@gmail.com>
2022-08-11 11:22:46 -0300

    Add uiviewcontroller extension

---
+++ minigames/Extensions/UiViewControllerExtension+.swift
@@ -0,0 +1,19 @@
+import UIKit
+import SpriteKit
+
+extension UIViewController {
+    func openScene(scene: SKScene?) {
+        self.view = SKView()
+        self.view.bounds = UIScreen.main.bounds
+        
+        if let view = self.view as? SKView {
+            if let scene = scene {
+                scene.size = CGSize(width: 750, height: 1334)
+                scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+                view.contentMode = .scaleAspectFit
+                view.presentScene(scene)
+            }
+        }
+    }
+}
+
